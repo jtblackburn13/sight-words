d5118b51692cb4e43ab1a9c394b7733d7c2c7995
Tyler Blackburn <jtblackburn13@yahoo.com>
2020-03-19 02:31:33 -0500

    adding amplify

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      word: "",
+      wordList: [
+        "I",
+        "we",
+        "in",
+        "like",
+        "mom",
+        "is",
+        "it",
+        "my",
+        "me",
+        "to",
+        "can",
+        "the",
+        "and",
+        "he",
+        "look",
+        "but",
+        "see",
+        "she",
+        "said",
+        "are",
+        "was",
+        "with",
+        "play",
+        "little",
+        "love",
+        "you",
+        "some",
+        "come",
+        "have",
+        "so",
+        "here",
+        "go",
+        "of",
+        "they",
+        "out",
+        "our",
+        "from",
+        "want",
+        "your",
+        "could",
+        "would",
+        "went",
+        "up",
+        "has",
+        "do",
+        "at",
+        "for",
+        "this",
+        "had",
+        "her",
+        "him",
+        "his",
+        "that",
+        "which",
+        "one",
+        "were"
+      ]
+    };
+  };
+
+
+  handleClick() {
+    let wordList = this.state.wordList;
+    let index = Math.floor(Math.random() * wordList.length)
+    console.log(`index: ${index}`);
+    this.setState({
+      word: wordList[index]
+    })
+  }
+
+
+
+
+  render() {
+    return (
+      <div style={{ height: "100vh", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "navy" }}>
+        <div onClick={() => { this.handleClick() }} style={{ height: "80vh", width: "80vw", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "black", border: "3px solid blue" }} >
+          <h1 style={{ color: "white", textAlign: "center", fontSize: "15vw" }}>
+            {this.state.word}
+          </h1>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
